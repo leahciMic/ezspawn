11bff968a7508207a307b02c05f2540fa0e1f3c8
Michael Leaney <mleaney@tokbox.com>
2015-09-11 11:36:54 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# ezspawn
+
+Spawn a process with ease.
+
+## Install
+
+```sh
+npm install --save ezspawn
+```
+
+## Usage
+
+```js
+var ezspawn = require('ezspawn');
+
+ezspawn('ls -lh').then(function(output) {
+  console.log(output);
+});
+```
+
+## API
+
+`ezspawn(cmd, waitForProcess=true, timeout=60000)`
+
+Runs `cmd`, and returns a promise with the results.
+
+### `cmd`
+
+The command you wish to execute. It uses
+(cli-command-parser)[https://www.npmjs.com/package/cli-command-parser] to parse
+the string into a suitable format for `child_process#spawn`.
+
+### `waitForProcess`
+
+When true, ezspawn will capture stdout, stderr and wait for the process to
+terminate. The returned promise will be resolved to an object containing the
+following:
+
+```js
+{
+  code: exitCode,
+  stdout: stdoutBuffer,
+  stderr: stderrBuffer
+};
+```
+
+When false, the promise will be resolved with the return value of
+`child_process#spawn`.
+
+Default: `true`
+
+### `timeout`
+
+Specifies how long we should wait for the process in milliseconds before timing
+out.
+
+Default: 60 seconds
---
+++ index.js
@@ -0,0 +1,64 @@
+var debug = require('debug')('ezspawn:debug');
+var warn = require('debug')('ezspawn:warn');
+var cliCommandParser = require('cli-command-parser');
+var bluebird = require('bluebird');
+
+module.exports = function(cmd, waitForProcess, timeout) {
+  debug('Execute: ' + cmd);
+
+  if (typeof waitForProcess === 'undefined') {
+    waitForProcess = true;
+  }
+
+  if (typeof timeout === 'undefined') {
+    timeout = 60000; // 60 seconds
+  }
+
+  var args = cliCommandParser(cmd);
+  var exe = args.shift();
+
+  var proc = spawn(exe, args);
+
+  if (waitForProcess) {
+    debug('Waiting for ' + cmd + ' to finish');
+    var stdoutBuffer = '';
+    var stderrBuffer = '';
+
+    proc.stdout.on('data', function(data) {
+      debug('stdout: ' + data);
+      stdoutBuffer += data;
+    });
+
+    proc.stderr.on('data', function(data) {
+      warn('stderr: ' + data);
+      stderrBuffer += data;
+    });
+
+    var promise = new bluebird.Promise(function(resolve, reject) {
+      proc.on('error', reject);
+      proc.on('close', function(exitCode) {
+        debug(cmd + ' completed with exit code: ' + exitCode);
+        var result = {
+          code: exitCode,
+          stdout: stdoutBuffer,
+          stderr: stderrBuffer
+        };
+
+        if (exitCode === 0) {
+          return resolve(result);
+        }
+
+        return reject(result);
+      });
+    });
+
+    if (timeout > 0) {
+      promise = promise.timeout(timeout)
+        .catch(function(function(e) {
+          proc.kill();
+        });
+    }
+  }
+
+  return new bluebird.resolve(proc);
+};
