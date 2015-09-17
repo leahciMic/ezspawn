af183e667f34de56a3ef8cccfd942b6987fb2ba1
Michael Leaney <mleaney@tokbox.com>
2015-09-17 16:07:33 +1000

    Log command timed out

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ module.exports = function(cmd, waitForProcess, timeout) {
       promise = promise.timeout(timeout)
         .catch(function(e) {
           proc.kill();
+          debug(cmd + ' timed out after ' + timeout + 'ms');
           throw e;
         });
     }
